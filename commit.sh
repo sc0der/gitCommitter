e093ab752c70684a2e41f9d0fdce2bc84a84f648
Sirius <siriusproger@gmail.com>
2022-09-20 22:48:01 +0500

    added function get commit message

--- commit.sh
+++ commit.sh
@@ -1,13 +1,26 @@
 # !/bin/bash
 
+
+
+getCommitMessage(){
+    if [[ "$1" != '' ]];then
+        echo "Salom $1"
+    else
+        echo "Please enter a message: "
+        read message
+        echo "Messsage is: $message"
+    fi
+}
+
 checkIfDefaultBranchSet()
 {
     if [ "$DEFAULT_BRANCH" != "" ];then
-        echo "Kiki"
+        getCommitMessage
     else
         echo "You didn't set DEFAULT_BRANCH"
         echo "please run command 'export DEFAULT_BRANCH=<your branch>'"
     fi
 }
+
 
 checkIfDefaultBranchSet
